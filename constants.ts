a32182ab4cacb5ade73aaf6da043db5cd60c946a
kwoncho1001 <66509033+kwoncho1001@users.noreply.github.com>
2026-02-21 15:17:43 +0900

    feat: Update student response URL
    Replaced the old Google Apps Script URL for student responses with a newly deployed one. This ensures the application uses the correct endpoint for submitting student data.

--- constants.ts
+++ constants.ts
@@ -1,7 +1,8 @@
-
-
 // constants.ts
 
 export const QUESTION_DB_URL = 'https://script.google.com/macros/s/AKfycbwasbL-XWmiJ1oVpgXVJDMVi5ckfthJ2oe1m9-Kr3lAfhvGInq732UH6Zk1x1kQnPO8Xg/exec';
-export const STUDENT_RESPONSE_URL = 'https://script.google.com/macros/s/AKfycbzOJNeshDQcaixH3adTHVSTopO5_Q6oKnANW-0xjSx2rLqM1fqNRBTFfzXK-pMQHTOW/exec';
+
+// 학원 계정으로 새로 배포된 URL로 교체 완료
+export const STUDENT_RESPONSE_URL = 'https://script.google.com/macros/s/AKfycbwDQ7dYYMJE0fIo9cIeMhrJ4XUAZQ5wzPQdCMcEQlKAQyoPx5scoTpCMyGVq3Djx8kQ/exec';
+
 export const CLASSIFICATION_CSV_URL = 'https://script.google.com/macros/s/AKfycbxrUcgsRIEXnX-JwBHls-zLLTrLB-og-NJ6u7kEOso2SC5aYOFbFeZh2Y1TFUDjO30PoA/exec';
